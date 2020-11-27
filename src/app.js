a63680a39d64056370ea84ff56f6046c1e4440aa
Edmar159 <dema159@msn.com>
2020-11-27 06:21:46 -0300

    Update app.js
    Retornando a lógica antiga, problema acontecia na mensagem de erro.

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ function validateId(request,response,next){
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
     
   if (repositoryIndex < 0){
-      return response.status(400).json({error: 'Repository not found!'});
+      return response.status(400).json({error: 'Repository does not exists.'});
   }
 
   next();
@@ -73,30 +73,22 @@ app.put("/repositories/:id", validateId,validateUrlUpdate, (request, response) =
   // PUT /repositories/:id: A rota deve alterar apenas o title, a url e as techs do repositório que 
   // possua o id igual ao id presente nos parâmetros da rota;
   const {id} = request.params;
-  const {title, url, techs, likes} = request.body;
+  const {title, url, techs} = request.body;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  console.log(likes);
-  
-  if(likes > 0){
-    const repository = {
-      likes: repositories[repositoryIndex].likes}
-      
-    return response.json(repository);
-  }else{
-    const repository = {
-        id,
-        title: title ? title !== undefined : repositories[repositoryIndex].title,
-        url: url ? url !== undefined : repositories[repositoryIndex].url,
-        techs: techs ? techs !== undefined : repositories[repositoryIndex].techs,
-        likes: repositories[repositoryIndex].likes
-    }
-    
-    repositories[repositoryIndex] = repository;
-
-    return response.json(repository);
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
   }
+    
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+  
 });
 
 app.delete("/repositories/:id", validateId, (request, response) => {
